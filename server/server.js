7b2cc7888308703e00e19a428a3c5b71b042ab30
yesaswini19 <yesaswinipamidi19@gmail.com>
2025-10-18 11:34:10 +0530

    Deploying latest version of the API

--- server/server.js
+++ server/server.js
@@ -102,7 +102,7 @@ mongoose.connect(MONGO_URI)
     .then(() => {
         console.log('MongoDB connected successfully.');
         server.listen(PORT, () => {
-            console.log("Server is running on port ${PORT}");
+            console.log(`Server is running on port ${PORT}`);
         });
     })
     .catch(err => {
